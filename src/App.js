4f331744e695293d7a2bab8d1ee4c6baad5c9afc
Bhansingh <bhanusingh6207@gmail.com>
2022-04-06 22:29:41 +0530

    quiz app creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
 
 function App() {
+  const moneyPyramid = [
+    {
+      id: 1,
+      amount: "$ 100",
+    },
+    {
+      id: 2,
+      amount: "$ 500",
+    },
+    {
+      id: 3,
+      amount: "$ 1000",
+    },
+    {
+      id: 4,
+      amount: "$ 1500",
+    },
+    {
+      id: 5,
+      amount: "$ 2000",
+    },
+    {
+      id: 6,
+      amount: "$ 2500",
+    },
+    {
+      id: 7,
+      amount: "$ 3000",
+    },
+    {
+      id: 8,
+      amount: "$ 35000",
+    },
+    {
+      id: 9,
+      amount: "$ 450000",
+    },
+    {
+      id: 10,
+      amount: "$ 50000",
+    },
+    {
+      id: 11,
+      amount: "$ 80000",
+    },
+    {
+      id: 12,
+      amount: "$ 10000000",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main">Main</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem active">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">$ 400</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
